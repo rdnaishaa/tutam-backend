7077cee24569710ded7135b5d7e0631bc6f69ab2
DESKTOP-EV1O5OI\USER <raishasyauqi7@gmail.com>
2025-05-04 15:42:19 +0700

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
